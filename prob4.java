b13c05f473b43f88686a182f4e9b10698ddc9917
TheVibinBear_ <69886633+thevibinbear@users.noreply.github.com>
2022-12-10 20:45:22 -0800

    Update prob4.java

--- prob4.java
+++ prob4.java
@@ -9,16 +9,16 @@ public class prob4 {
         attack = 0;
         defense = 0;
         speed = 0;
-        total = t;
+        total = 0;
     }
-    public prob4(int h, int a, int d, int s, int t) {
+    public prob4(int h, int a, int d, int s) {
         if (h < 255 && a < 255 && d < 255 && s < 255) {
-            if (t <= 510) {
+            if ( (h + a + d + s) <= 510) {
                 hp = h;
                 attack = a;
                 defense = d;
                 speed = s;
-                total = t;
+                total = h + a + d + s;
             }
         }
     }
