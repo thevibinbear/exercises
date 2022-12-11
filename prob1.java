fdb6f18ec9255ad0d505e6eefc3b71fc4322f773
TheVibinBear_ <69886633+thevibinbear@users.noreply.github.com>
2022-12-10 20:20:23 -0800

    Add files via upload

---
+++ prob1.java
@@ -0,0 +1,13 @@
+public class prob1 {
+    private boolean isFishing;
+    private boolean isFighting;
+    private int hitPoints;
+    public prob1 (boolean isFishing, boolean isFighting, int hitPoints) {
+        this.isFishing = isFishing;
+        this.isFighting = isFighting;
+        this.hitPoints = hitPoints;
+    }
+    public void subtractHealth(int amountOfDamage) {
+        hitPoints -= amouuntOfDamage;
+    }
+}
---
+++ prob2.java
@@ -0,0 +1,26 @@
+public class prob2 {
+    private int hp;
+    private int attack;
+    private int defense;
+    private int speed;
+    private int total;
+
+    public prob2 () {
+        hp = 0;
+        attack = 0;
+        defense = 0;
+        speed = 0;
+    }
+    public prob2 (int h, int a, int d, int s) {
+        hp = h;
+        attack = a;
+        defense = d;
+        speed = s;
+    }
+    public void calculateTotal() {
+        total = hp + attack + defense + speed;
+    }
+    public int getTotal() {
+        return total;
+    }
+}
