407dc3dbb40a803293994f120a4f532a55c1ccbc
TheVibinBear_ <69886633+thevibinbear@users.noreply.github.com>
2022-12-10 20:55:13 -0800

    Update Trainer.java

--- Trainer.java
+++ Trainer.java
@@ -1,4 +1,36 @@
 public class Trainer {
-    public Trainer(String f, String l, String m, boolean hasPlayed) {
+    String firstName;
+    String lastName;
+    String middleName;
+    boolean hasPlayed;
+    public Trainer(String f, String l, String m, boolean p) {
+        firstName = f;
+        lastName = l;
+        middleName = m;
+        hasPlayed = p;
+    }
+    public String getFirstName () {
+        return firstName;
+    }
+    public String getLastName () {
+        return lastName;
+    }
+    public String getMiddleName () {
+        return middleName;
+    }
+    public boolean getHasPlayed () {
+        return hasPlayed;
+    }
+    public void setFirstName(String f) {
+        firstName = f;
+    }
+    public void setLastName(String l) {
+        lastName = l;
+    }
+    public void setMiddleName(String m) {
+        middleName = m;
+    }
+    public void setPlayed(String p) {
+        setPlayed = p;
     }
 }
